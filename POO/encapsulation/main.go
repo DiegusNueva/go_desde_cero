acd5e4f3754bcc236c01ec08ef8f3de5662bb9f2
Diego <diegusnueva@gmail.com>
2023-08-13 15:27:47 +0200

    Finalizamos la encapsulación

--- POO/encapsulation/main.go
+++ POO/encapsulation/main.go
@@ -2,34 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/DiegusNueva/go_desde_cero/tree/main/POO/encapsulation/course"
 )
 
 func main() {
-
-	Go := &Course{
-		"Go desde Cero",
-		12.34,
-		false,
-		[]uint{12, 56, 89},
-		map[uint]string{
-			1: "Introducción",
-			2: "Estructuras",
-			3: "Maps",
-		},
-	}
-
-	Go.ChangePrice(67.01)
-	fmt.Println(Go.Price)
+	Go := course.New("Go desde Cero", 12.34, false)
+	Go.SetUserIDs([]uint{12, 56, 89})
+	Go.SetClasses(map[uint]string{
+		1: "Introducción",
+		2: "Estructuras",
+		3: "Maps",
+	})
 
 	Go.PrintClasses()
-
-	// css := Course{Name: "CSS desde cero", IsFree: true}
-	// js := Course{}
-	// js.Name = "Curso JS"
-	// js.UserIDs = []uint{12, 67}
-
-	// fmt.Println(Go.Name)
-	// fmt.Printf("%+v\n", css)
-	// fmt.Printf("%+v\n", js)
-
+	Go.SetName("POO con Go")
+	fmt.Println(Go.Name())
 }
