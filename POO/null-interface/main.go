021e90c40eff4b4cca06d08f9faec9e2aae38e2f
Diego <diegusnueva@gmail.com>
2023-08-13 23:48:05 +0200

    Finish

--- POO/null-interface/main.go
+++ POO/null-interface/main.go
@@ -1,14 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type exampler interface {
 	x()
 }
 
 func wrapper(i interface{}) {
-	fmt.Printf("valor: %v, Tipo: %T\n", i, i)
+	//fmt.Printf("valor: %v, Tipo: %T\n", i, i)
 
+	// v, ok := i.(string)
+
+	// if ok {
+	// 	fmt.Println(strings.ToUpper(v))
+	// }
+
+	switch v := i.(type) {
+
+	case string:
+		fmt.Println(strings.ToUpper(v))
+	case int:
+		fmt.Println(v * 2)
+	default:
+		fmt.Println("valor: %v, Tipo: %T\n", i, i)
+	}
 }
 
 func main() {
@@ -16,5 +34,7 @@ func main() {
 	// e.x()
 	wrapper(12.34)
 	wrapper(false)
+	wrapper("Diego")
+	wrapper("Álvaro")
 
 }
