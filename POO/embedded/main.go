867238bacbf2b6b946336891f23908c9d352f846
Diego <diegusnueva@gmail.com>
2023-08-13 22:22:20 +0200

    Finish

--- POO/embedded/main.go
+++ POO/embedded/main.go
@@ -15,13 +15,27 @@ func (p Person) Greet() {
 	fmt.Println("Hello")
 }
 
+type Human struct {
+	Age      uint
+	Children uint
+}
+
+func NewHuman(age, children uint) Human {
+	return Human{age, children}
+}
+
 type Employee struct {
 	Person
+	Human
 	Salary float64
 }
 
-func NewEmployee(name string, age uint, salary float64) Employee {
-	return Employee{NewPerson(name, age), salary}
+func (p Employee) Greet() {
+	fmt.Println("Hola desde empleado")
+}
+
+func NewEmployee(name string, age uint, childen uint, salary float64) Employee {
+	return Employee{NewPerson(name, age), NewHuman(age, childen), salary}
 }
 
 func (e Employee) Payroll() {
@@ -29,6 +43,9 @@ func (e Employee) Payroll() {
 }
 
 func main() {
-	e := NewEmployee("Alejandro", 30, 1000000)
+	e := NewEmployee("Alejandro", 30, 2, 1000000)
+	fmt.Println(e.Name, e.Human.Age)
+	e.Person.Greet()
+	e.Greet()
 	e.Payroll()
 }
